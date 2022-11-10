e3dde0dfc47ee4ed8cbb02efa56befe204697606
Malathi Soundarapandian <37123323+malathisoundarapandian@users.noreply.github.com>
2022-11-10 14:12:39 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,4 +12,4 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 fruits_selected = streamlit.multiselect('pick some fruits',list(my_fruit_list.index),['Avocado','Strawberries'])
 
 
-streamlit.dataframe(fruits_to_show)
+streamlit.dataframe(fruits_selected)
